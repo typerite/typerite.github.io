6a4314fa03fd7b7ce88383f4de8c1fd66d57d7ff
lockness-Ko <videolk9373@gmail.com>
2021-08-28 17:18:46 +1000

    indexedDB working

--- js/editor.js
+++ js/editor.js
@@ -1,28 +1,29 @@
 let textbox = document.querySelector("#text");
 let title = document.querySelector("#title");
+let prevs = document.querySelector("#docprevs");
 
 let fontSize = 16
 
 var isCtrl = false;
-document.onkeyup=function(e){
-    if(e.keyCode == 17) isCtrl=false;
+document.onkeyup = function (e) {
+    if (e.keyCode == 17) isCtrl = false;
 }
 
-document.onkeydown=function(e){
-    if(e.keyCode == 17) isCtrl=true;
+document.onkeydown = function (e) {
+    if (e.keyCode == 17) isCtrl = true;
     // console.log(e.keyCode)
-    if(e.keyCode == 83 && isCtrl == true) {//Ctrl+S
-        download(title.value+".txt", textbox.innerText);
+    if (e.keyCode == 83 && isCtrl == true) { //Ctrl+S
+        download(title.value + ".txt", textbox.innerText);
         return false;
     }
-    if(e.keyCode == 219 && isCtrl == true) {//Ctrl+[
+    if (e.keyCode == 219 && isCtrl == true) { //Ctrl+[
         fontSize--;
-        textbox.style.fontSize = fontSize+"px";
+        textbox.style.fontSize = fontSize + "px";
         return false;
     }
-    if(e.keyCode == 221 && isCtrl == true) { //Ctrl+]
+    if (e.keyCode == 221 && isCtrl == true) { //Ctrl+]
         fontSize++;
-        textbox.style.fontSize = fontSize+"px";
+        textbox.style.fontSize = fontSize + "px";
         return false;
     }
 }
@@ -31,11 +32,79 @@ function download(filename, text) {
     var element = document.createElement('a');
     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
     element.setAttribute('download', filename);
-  
+
     element.style.display = 'none';
     document.body.appendChild(element);
-  
+
     element.click();
-  
+
     // document.body.removeChild(element);
-  }
+}
+
+// INDEXEDDB STUFF!!
+
+window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB ||
+    window.msIndexedDB;
+
+window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction ||
+    window.msIDBTransaction;
+window.IDBKeyRange = window.IDBKeyRange ||
+    window.webkitIDBKeyRange || window.msIDBKeyRange
+
+if (!window.indexedDB) {
+    window.alert("Your browser doesn't support a stable version of IndexedDB.")
+}
+
+let db;
+let request = window.indexedDB.open("typerite", 3);
+
+request.onerror = function (event) {
+    console.log("error: ");
+};
+
+
+request.onsuccess = function (event) {
+    db = request.result;
+    console.log("success: " + db);
+};
+
+request.onupgradeneeded = function (event) {
+    var db = event.target.result;
+    db.createObjectStore("typerite", {
+        keyPath: "id"
+    });
+}
+
+function add(id_, title_, text_) {
+    var request = db.transaction(["typerite"], "readwrite").objectStore("typerite").put({
+        id: id_,
+        title: title_,
+        text: text_
+    });
+
+    request.onsuccess = function (event) {
+        // alert("Added to database");
+    };
+
+    request.onerror = function (event) {
+        alert("Unable to add data");
+    }
+}
+
+let objs = []
+
+function readAll() {
+    let objectStore = db.transaction("typerite").objectStore("typerite");
+    objs = [];
+    objectStore.openCursor().onsuccess = function (event) {
+        var cursor = event.target.result;
+
+        if (cursor) {
+            // alert("Title " + cursor.value.title);
+            objs.push(cursor.value)
+            cursor.continue();
+        } else {
+            // alert("No more entries!");
+        }
+    };
+}
--- js/type.js
+++ js/type.js
@@ -1,3 +1,6 @@
+let prevs = document.querySelector("#docprevs");
+
+
 function drop() {
     document.querySelector(".left__toolbar1").classList.toggle("show");
 }
@@ -6,16 +9,86 @@ function drop1() {
     document.querySelector(".left__toolbar2").classList.toggle("show");
 }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (e) {
-    if (!e.target.matches('#drop')) {
-        var myDropdown = document.getElementById(".left__toolbar2");
-        if (myDropdown.classList.contains('show')) {
-            myDropdown.classList.remove('show');
-        }
-        var myDropdown = document.getElementById(".left__toolbar1");
-        if (myDropdown.classList.contains('show')) {
-            myDropdown.classList.remove('show');
-        }
+window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB ||
+    window.msIndexedDB;
+
+window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction ||
+    window.msIDBTransaction;
+window.IDBKeyRange = window.IDBKeyRange ||
+    window.webkitIDBKeyRange || window.msIDBKeyRange
+
+if (!window.indexedDB) {
+    window.alert("Your browser doesn't support a stable version of IndexedDB.")
+}
+
+let db;
+let request = window.indexedDB.open("typerite", 3);
+
+request.onerror = function (event) {
+    console.log("error: ");
+};
+
+
+request.onsuccess = function (event) {
+    db = request.result;
+    console.log("success: " + db);
+};
+
+request.onupgradeneeded = function (event) {
+    var db = event.target.result;
+    db.createObjectStore("typerite", {
+        keyPath: "id"
+    });
+}
+
+function add(id_, title_, text_) {
+    var request = db.transaction(["typerite"], "readwrite").objectStore("typerite").put({
+        id: id_,
+        title: title_,
+        text: text_
+    });
+
+    request.onsuccess = function (event) {
+        // alert("Added to database");
+    };
+
+    request.onerror = function (event) {
+        alert("Unable to add data");
     }
+}
+
+let objs = []
+
+function readAll() {
+    let objectStore = db.transaction("typerite").objectStore("typerite");
+    objs = [];
+    objectStore.openCursor().onsuccess = function (event) {
+        var cursor = event.target.result;
+
+        if (cursor) {
+            // alert("Title " + cursor.value.title);
+            objs.push(cursor.value)
+            cursor.continue();
+        } else {
+            // alert("No more entries!");
+        }
+    };
+}
+
+window.onload = () => {
+    readAll()
+    setTimeout(() => {
+        // alert(objs[0]["title"])
+        objs.forEach((i, j) => {
+            c = document.createElement("div");
+            c.className = "left__list__doc";
+            c.innerHTML = `<div class="left__list__doc__title">
+            `+i["title"]+`
+        </div>
+        <div class="left__list__doc__text">
+        `+i["text"].match(/.{1,25}(\s|$)/g)[0].replace(/^\s+|\s+$/g, '')+`...
+        </div>`;
+        });
+        prevs.appendChild(c);
+    }, 100);
 }
